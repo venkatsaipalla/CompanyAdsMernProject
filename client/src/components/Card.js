11ee61e972859fcce3ec37fb4c0a2082f22688e3
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-11-10 15:34:37 +0530

    Add files via upload

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,7 +8,6 @@ const {cta} =companyDetails;
         <div class="container">
   <h1>{companyDetails.companyId.name} </h1>
   <div class="img-box"  style={{backgroundImage: `url(${companyDetails.imageUrl})`}}>
-    {/* <img src={companyDetails.imageUrl} alt-text="Image"/> */}
     <div class="cta">
        <h3 className="card-title">{companyDetails.primaryText}</h3>
       <a href={companyDetails.companyId.url} target="_blank">{cta ? cta :'Click here' }</a>
@@ -16,16 +15,7 @@ const {cta} =companyDetails;
   </div>
     <h5 className="card-title">{companyDetails.description}</h5>
 </div>
-        {/* <div className="card" style={{width: "18rem",border:"2px solid black"}}>
-        <img className="card-img-top image" src={companyDetails.imageUrl} alt-text="Card image cap" style={{width:"18rem",height:"13rem"}}/>
-            <h5 className="card-title">{companyDetails.description}</h5>
-            <p className="card-text">{companyDetails.primaryText}</p>
-            <p className='company-name'>{companyDetails.companyId.name} </p>
-          <div className="card-body">
-            <a href={companyDetails.companyId.url} target="_blank" className="button">{cta ? cta :'Click here' }</a>
-          </div>
-        </div> */}
-        </section>
+     </section>
     </Fragment>
   )
 }
